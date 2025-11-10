84c86422936fe127b8ca7c20382ace82c017c245
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-10 23:00:09 +0530

    page-not-found routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { About } from './about/about';
 import { Login } from './login/login';
 import { Contact } from './contact/contact';
 import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
     {path:'about',component:About},
     {path:'login',component:Login},
-    {path:'contact',component:Contact},
-    {path:'',component:Home}
+    {path:'',component:Home},
+    {path:'**',component:PageNotFound}
 ];
