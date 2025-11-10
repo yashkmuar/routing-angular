fe95b9a52c58330fc3c73f4e29cc269fed4055b5
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-10 22:30:41 +0530

    header navigation bar now clickable

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 
 @Component({
