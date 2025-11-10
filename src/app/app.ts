e25de07dce7e719429dcc4c38ce91ac1a3a55820
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-10 22:19:20 +0530

    Routing implemented

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  //imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('routing-angular');
+
 }
