3f1af53c54f1ba51f7ae9587ea7f69d40c59d2ae
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-10 23:07:08 +0530

    profile routing added in home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { Login } from './login/login';
 import { Contact } from './contact/contact';
 import { Home } from './home/home';
 import { PageNotFound } from './page-not-found/page-not-found';
+import { Profile } from './profile/profile';
 
 export const routes: Routes = [
     {path:'about',component:About},
     {path:'login',component:Login},
     {path:'',component:Home},
+    {path:'profile',component:Profile},
     {path:'**',component:PageNotFound}
 ];
