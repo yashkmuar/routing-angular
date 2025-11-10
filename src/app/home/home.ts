016657fc9a2c045cfd81b29d5eeb66df052c2dd9
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-10 23:26:51 +0530

    passing data with button click

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -9,4 +9,10 @@ import { RouterLink } from "@angular/router";
 })
 export class Home {
 
+  constructor(private router:Router){
+
+  }
+  goToProfile(){
+    this.router.navigate(['/profile',{name:'Yash Kumar'}]);
+  }
 }
