2e4b5387f67515e515edde2ba72c1eb53e669540
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-10 22:52:23 +0530

    page-not-found component added

---
+++ src/app/page-not-found/page-not-found.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageNotFound } from './page-not-found';
+
+describe('PageNotFound', () => {
+  let component: PageNotFound;
+  let fixture: ComponentFixture<PageNotFound>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PageNotFound]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PageNotFound);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
