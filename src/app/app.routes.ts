2e530e5149a622e3016c676abf002e90ec9bab98
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-10 22:42:32 +0530

    home component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Login } from './login/login';
 import { Contact } from './contact/contact';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     {path:'about',component:About},
     {path:'login',component:Login},
-    {path:'contact',component:Contact}
+    {path:'contact',component:Contact},
+    {path:'',component:Home}
 ];
