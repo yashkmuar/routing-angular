104e985e1c8ed2b8fde0b6e911e277098c1e25f9
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-10 22:28:56 +0530

    Header navigation bar started

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
