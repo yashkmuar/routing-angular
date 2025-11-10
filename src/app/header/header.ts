f1753f2c0b9129fdbc70f6aae075ee719d0f7733
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-10 22:47:42 +0530

    routerLinkActive added , routerLinkActiveOptions added for home page

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],  
+  imports: [RouterLink, RouterLinkActive],  
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
